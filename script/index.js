ba9064a3b36e1c80c3346f25ace16c739ad13601
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-17 15:26:37 +0200

    completato milestone 2: effettuato then, catch e finally

--- script/index.js
+++ script/index.js
@@ -3,3 +3,34 @@
 // collegamento con l'API (vedi postman anche)
 const indirizzo_api = "https://lanciweb.github.io/demo/api/pictures/";
 
+
+// chiamata verso l'indirizzo api
+fetch(indirizzo_api)
+    //then gestisce risposta del server 
+    .then(function (response) {
+
+      /*   //se la risposta non è ok, allora lancio un errore
+        if (!response.ok) {
+            console.error("Errore nella risposta del server");
+        } */
+        
+        //se la risposta è ok, allora la trasformo in json
+        return response.json();
+
+    })
+
+    //then gestisce la risposta in json dal passaggio precedente
+    .then(function (dati) {
+        console.log("dati recuperati", dati);  // qui vediamo array con le card
+
+    })
+
+    // catch gestisce errori nella richiesta
+    .catch(function (error) {
+        console.error("Errore nella richiesta:", error);
+    })
+    
+    // il finally viene eseguito sempre alla fine, sia in caso di successo che di errore
+    .finally(function () {
+        console.log("Richiesta completata");
+    });
