8afe53f4361a8f422f5443eddcc971bdac5abe8c
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-18 16:08:46 +0200

    completato richieste base senza bonus

--- script/index.js
+++ script/index.js
@@ -2,6 +2,8 @@
 
 // collegamento con l'API (vedi postman anche)
 const indirizzo_api = "https://lanciweb.github.io/demo/api/pictures/";
+// collegamento con il contenitore nel DOM dove inserire le card
+const contenitore = document.querySelector(".contenitore");
 
 
 // chiamata verso l'indirizzo api
@@ -22,8 +24,26 @@ fetch(indirizzo_api)
     //then gestisce la risposta in json dal passaggio precedente
     .then(function (dati) {
         console.log("dati recuperati", dati);  // qui vediamo array con le card
+            
+        dati.forEach(function (card) {  // per ogni card nell'array, eseguo questa funzione
 
-    })
+
+              /*   Quando voglio inserire un dato variabile preso dal server, uso il simbolo ${}
+             Ad esempio, ${card.url} verra sostituito dal vero link dell'immagine.
+             ${card.title} verra sostituito dalla vera didascalia. */
+
+            const carta_html = `
+        <div class="scheda_polaroid">
+            <img src="./assets_day1/img/pin.svg" alt="Spillo rosso" class="spilla_fissaggio">
+            <img src="${card.url}" alt="${card.title}" class="immagine_card">
+            <p class="testo_didascalia">${card.title}</p>
+        </div>
+        `;  // creo una stringa con il codice html per ogni card, usando i dati dell'api
+
+    // aggiungo il codice html della card al contenitore, usando += per non sovrascrivere le card già presenti
+    contenitore.innerHTML += carta_html;  
+    });
+})
 
     // catch gestisce errori nella richiesta
     .catch(function (error) {
